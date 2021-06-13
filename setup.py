594544915056930ff5e009c42d9ff233af3c0e40
Jwizzle <info@hossel.net>
2021-06-13 14:32:28 +0200

    Hotfix yml providers not loading

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jwizzle/cws",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'requests',
         'pyyaml',
